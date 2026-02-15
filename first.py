ac68324bc6f94ea71db551eec9782e14f0f31c5d
M Hussain Shahzad <hussainshahzad7744@gmail.com>
2026-02-15 17:31:44 +0500

    Variable in python and (Hellow world)

---
+++ first.py
@@ -0,0 +1,37 @@
+
+#Hello world in python
+print("Hello world")
+
+#Variables in python
+
+# 5 types of variables in python
+# 1. String
+# 2. Integer
+# 3. Float
+# 4. Boolean    
+# 5. undefined
+
+# String variable
+name="Python"  # String variable
+print(name)
+print(type(name))
+
+
+# Integer variable
+age=30
+print(age)
+print(type(age))    
+
+# Float variable
+pi=3.14
+print(pi)   
+
+# Boolean variable
+is_python_fun=True      
+print(is_python_fun)
+print(type(is_python_fun))
+
+# Undefined variable
+undefined_variable=None
+print(undefined_variable)
+print(type(undefined_variable))
