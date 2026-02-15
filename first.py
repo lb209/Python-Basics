56b89bf2ac22a44e38e368f3260a39098422d0a8
M Hussain Shahzad <hussainshahzad7744@gmail.com>
2026-02-15 17:42:33 +0500

    operator and variable in python

--- first.py
+++ first.py
@@ -1,56 +1,22 @@
-#Operators in Python
+#variable and operators
 
-#Arithmetic Operators
-a = 10
-b = 5   
-print("Addition:", a + b)          # Output: 15
-print("Subtraction:", a - b)       # Output: 5
-print("Multiplication:", a * b)    # Output: 50
-print("Division:", a / b)          # Output: 2.0
-print("Floor Division:", a // b)   # Output: 2
-print("Modulus:", a % b)          # Output: 0
-print("Exponentiation:", a ** b)   # Output: 100000
+number1 = 10
+number2 = 20
+print(number1 + number2)
 
+num=10
+count=20+num
+print(count)
 
-#Relational Operators
-x = 10
-y = 20
-print("Equal to:", x == y)        # Output: False
-print("Not equal to:", x != y)    # Output: True
-print("Greater than:", x > y)      # Output: False          
-print("Less than:", x < y)         # Output: True
-print("Greater than or equal to:", x >= y)  # Output: False
-print("Less than or equal to:", x <= y)     # Output: True  
+#string
+name = "John"
+greeting = "Hello, " + name + "!"
+print(greeting)
 
-
-
-
-#Assignment Operators
-c = 10
-c += 5   # Equivalent to c = c + 5
-print("After += 5:", c)  # Output: 15
-c -= 3   # Equivalent to c = c - 3
-print("After -= 3:", c)  # Output: 12
-c *= 2   # Equivalent to c = c * 2
-print("After *= 2:", c)  # Output: 24
-c /= 4   # Equivalent to c = c / 4
-print("After /= 4:", c)  # Output: 6.0
-c //= 2  # Equivalent to c = c // 2
-print("After //= 2:", c) # Output: 3.0
-c %= 2   # Equivalent to c = c % 2
-print("After %= 2:", c)  # Output: 1.0
-c **= 3  # Equivalent to c = c ** 3
-print("After **= 3:", c) # Output: 1.0
-
-
-
-#Logical Operators
-p = True
-q = False
-print("Logical AND:", p and q)  # Output: False
-print("Logical OR:", p or q)    # Output: True          
-print("Logical NOT:", not p)    # Output: False
-
-
-
-
+#boolean
+is_raining = True
+if is_raining:
+    print("Don't forget to take an umbrella!")
+#list
+fruits = ["apple", "banana", "cherry"]
+print(fruits[0])  # Output: apple           
