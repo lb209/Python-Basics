5e98120066983a7fe2c344970444b142a1a4b00c
M Hussain Shahzad <hussainshahzad7744@gmail.com>
2026-02-18 15:41:41 +0500

    Tupled method in list

--- pro.py
+++ pro.py
@@ -1,70 +1,19 @@
-#List in ppython(Array)
+# Tupled Method
 
-#integer
-list=[1,2,3,4,5,6]
-print("store multipule value in single variable or list",list)
-print("#-----------#")
-#string
-str=["Apple","Mango","Bannana","Fruit"]
-stri=["hellow i am a python developer","hellow i am a web developer","hellow i am a app developer"]
-print(str)
-print(stri)
-print("#-----------#")
+tup=(1,2,3,4)
+tus=("go","gose","want")
+print(tup)
+print("This is valid method in tupled",tus)
+print("#----------#")
 
 
+#index tupled
+ind=(2,4,8,9,6)
+print("2 index is 8->",ind.index(8))
+print("#----------#")
 
+# count tuple
 
-
-
-
-#Type of List
-
-# 1 append list
-num=[1,2,3,4]
-num.append(5)
-print("add 5 in your list",num)
-print("#-----------#")
-# 2 sort list
-str=[4,5,1,2,7,9,5,6,10,11]
-striee=["d","s","a","b"]
-str.sort()
-striee.sort()
-print("your list is sorted ->",str)
-print("your list is sorted ->",striee)
-print("#-----------#")
-
-
-# 3 sort reverse list
-
-stri=[4,5,1,2,7,9,5,6,10,11]
-stri.sort(reverse=True)
-print("your list is decending sorted",stri)
-print("#-----------#")
-
-# 4 reverse list
-stri=[1,2,3,4,5,6]
-stri.reverse()
-print("your list is reverse",stri)
-print("#-----------#")
-
-
-# 5 insert list
-stri=[1,2,3,4,5,6]
-stri.insert(4,10)
-print("10 is insert in 4 index value",stri)
-print("#-----------#")
-
-
-# 6 remove list
-
-stri=[1,2,3,4,1,3]
-stri.remove(3)
-print("the 3 element is remove",stri)
-print("#-----------#")
-
-
-# 7 
-stri=[2,3,4,5,1]
-stri.pop(3)
-print("5 element is remoce",stri)
-
+ind=(2,4,8,9,6,8,77,8,99,8)
+print("the value is rwepeate->",ind.count(8))
+print("#----------#")
