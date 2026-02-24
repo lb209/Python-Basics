52993fd4aa92633686009ba6b02d2c8fe8ce3371
M Hussain Shahzad <hussainshahzad7744@gmail.com>
2026-02-24 19:19:22 +0500

    While loop in python

--- first.py
+++ first.py
@@ -1,15 +1,68 @@
-#input 
-name=input("Enter your name: ")
-print("Welcome",name)
+#while loop
+#print 5 number using while loop only
+
+i=0
+while i<=5:
+    print(i)
+    i+=1
+print("*------------*")
 
 
-num1=int(input("Enter first number: "))
-num2=int(input("Enter second number: "))
-sum=num1+num2
-print("The sum is: ",sum)
+# i=100
+# while i>=0:
+#     print(i*3)
+#     i+=1
+
+print("*------------*")
+#print 3 of table using while loop
+i=0
+while i<=10:
+    print(i*3)
+    i+=1
+
+print("*------------*")
+
+#print element using while loop[1,4,9,16,25,36,49,50]
+
+i=[1,4,9,16,25,36,49,50]
+indx=0
+while indx<len(i):
+    print(i)
+    indx+=1 
+    print("*------------*")
+
+    inp= int(input("Enter any number"))
+    while inp>0:
+        print(inp)
+        inp-=1
+        print("*------------*")
+
+a = int(input("Enter first number: "))
+b = int(input("Enter second number: "))
+
+# while loop سے sum
+while b > 0:
+    a = a + 1
+    b = b - 1
+
+# print("Sum =", a)
+print("*------------*")
+use = int(input("Enter digit: "))
+
+while use > 0:
+    if use == 1:
+        print("loop is end")
+        break
+    else:
+        print("loop is not end press 1")
+        use = int(input("Enter digit again: "))
+        print("*------------*")
 
 
-a=float(input("Enter first number: "))
-b=float(input("Enter second number: "))
-product=a*b
-print("The product is: ",product)   
+prim=int(input("Check prime number"))
+while prim>0:
+    if (prim%2==0):
+     print("It is not prim")
+    else:"prim number"
+
+   
