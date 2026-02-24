a84e18a652c194365580ada9318d1f54d86da1de
M Hussain Shahzad <hussainshahzad7744@gmail.com>
2026-02-24 19:35:52 +0500

    for loop in python

--- first.py
+++ first.py
@@ -1,68 +1,26 @@
-#while loop
-#print 5 number using while loop only
-
-i=0
-while i<=5:
-    print(i)
-    i+=1
-print("*------------*")
-
-
-# i=100
-# while i>=0:
-#     print(i*3)
-#     i+=1
-
-print("*------------*")
-#print 3 of table using while loop
-i=0
-while i<=10:
-    print(i*3)
-    i+=1
-
-print("*------------*")
-
-#print element using while loop[1,4,9,16,25,36,49,50]
-
-i=[1,4,9,16,25,36,49,50]
-indx=0
-while indx<len(i):
-    print(i)
-    indx+=1 
-    print("*------------*")
-
-    inp= int(input("Enter any number"))
-    while inp>0:
-        print(inp)
-        inp-=1
-        print("*------------*")
-
-a = int(input("Enter first number: "))
-b = int(input("Enter second number: "))
-
-# while loop سے sum
-while b > 0:
-    a = a + 1
-    b = b - 1
-
-# print("Sum =", a)
-print("*------------*")
-use = int(input("Enter digit: "))
-
-while use > 0:
-    if use == 1:
-        print("loop is end")
-        break
-    else:
-        print("loop is not end press 1")
-        use = int(input("Enter digit again: "))
-        print("*------------*")
+# for loop in python
+num=[1,2,3,4,5,6]
+for pri in num:
+    print(pri)
+print("*--------*")
+#print string
+num=("Mango","Banana","orange")
+for pri in num:
+    print(pri)
+    print("*--------*")
 
 
-prim=int(input("Check prime number"))
-while prim>0:
-    if (prim%2==0):
-     print("It is not prim")
-    else:"prim number"
+    #search number
+    num=[1,2,3,4,5,6]
+    ch=5
+    ind=0
+    for cch in num:
+        if(cch==ch):
+            print("number found at index",ind)
+            break
+        ind+=1
+        print("*--------*")
 
-   
+    
+
+
