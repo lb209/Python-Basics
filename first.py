860196bbb04c9c6ab533e7b1231ec71aa8fca00e
M Hussain Shahzad <hussainshahzad7744@gmail.com>
2026-02-25 17:46:15 +0500

    function in python

--- first.py
+++ first.py
@@ -1,19 +1,46 @@
-#range in python
+#function in python
 
-for el in range(10):
-    print(el)
-    print("*-------*")
+def cal ():
+    print("hellow world")
 
-    for el in range(10,20):
-        print(el)
+cal()
+print("*----------*")
 
-    print("*-------*")
+#write a program to print length of the list
 
-    #print even number
-    for el in range(10,30,2):
-     print(el)
+cou=["Apple","Banana","Orange"]
+def cout():
+    print(len(cou))
+cout()
+
+print("*----------*")
+cou=["Apple","Banana","Orange"]
+def cout(list):
+  for item in list:
+     print(item,end=" ")
+cout(cou)
+print("*----------*") 
 
 
-     get=int(input("Enter number"))
-     for el in range(10,20):
-        print(get*el)
+
+
+#find factorial of n
+def cal(n):
+    fact=1
+    for i in range(1,n+1):
+        fact *=i
+    print(fact)
+cal(10)
+
+print("*----------*") 
+   
+
+inp=int(input("Check even and node"))
+def che ():
+    if inp%2==0:
+     print("Even")
+    else:
+      "ood"
+che()
+
+   
