6106e94d1177ab7f9a17827d68d4b591b73f3300
M Hussain Shahzad <hussainshahzad7744@gmail.com>
2026-03-04 20:31:47 +0500

    2)oop(class,object,instructor)in python

--- test.py
+++ test.py
@@ -1,33 +1,30 @@
-#Topic number 1
+class Bank:
+    def __init__(self):
+        self.__balance = 0   # private balance
 
-class Name: # it is claas
-    def __init__(self, name, age): #it is instructor
-        self.nam = name #it is variable of class
-        self.age = age
-
-s1 = Name("Ali", 33) #it is object
-print(s1.nam)#it is object
-print(s1.age)#it is object
-print("*--------*")
-
-#Topic number 2
-class Check: # it is claas
-    def __init__(self):#it is instructor
-        self.name="hassan"#it is variable of class
-        self.pas=12#it is variable of class
-    def login(self): #creat a function
-        uname=input("Enter name")
-        upas=int(input("Enter password")) 
-        if uname ==self.name and upas==self.pas:
-            print("Successfuly Login")
+    def add(self):
+        ino = int(input("Enter amount to deposit: "))
+        if ino > 0:
+            self.__balance += ino
+            print("Amount added successfully")
         else:
-            print("Invald name and pass...")  
-            
-s2 = Check() #it is object
-s2.login()#it is object
-print("*--------*")
+            print("Please enter valid amount")
+
+    def wit(self):
+        inon = int(input("Enter amount to withdraw: "))
+        if inon <= self.__balance:
+            self.__balance -= inon
+            print("Amount withdrawn successfully")
+        else:
+            print("Insufficient balance")
+
+    def show_balance(self):
+        print("Current Balance:", self.__balance)
 
 
-    
+# Object
+s1 = Bank()
 
-
+s1.add()
+s1.wit()
+s1.show_balance()
