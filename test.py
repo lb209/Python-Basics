a886414f7c3009b197b30209ce53f433f246a876
M Hussain Shahzad <hussainshahzad7744@gmail.com>
2026-03-12 11:59:11 +0500

    oops:( inheritance, polymorphism)in python

--- test.py
+++ test.py
@@ -1,16 +1,61 @@
-class A:
-    def __init__(self,name,age):
-        self.name=name
-        self.age=age
+# #problem 1
+# class Animal:
+#     def sound(self):
+#         print("Animal makes sound")
+# class Dog:
+#     def sound(self):
+#         print("Dog barks")       
+# marg=[Animal(),Dog()]                 
+# for a in marg:
+#     a.sound()
 
-       
-class B(A):
-    def gre(self):
-        super(). __init__(self.name,self.age)
-        print(self.name)
-        print(self.age)
 
-        print("get")
-        
-s=B("HY",22)
-s.gre()
+#problem 2
+
+# class A:
+#     def send(self):
+#         print("Vechical moves")
+# class B(A):
+#     def send(self):
+#         super().send()
+#         print("car is moving")            
+
+# s=B()
+# s.send()       
+
+# problem 3
+# class Shape:
+#     def area(self):
+#         print("Shape area")
+
+# class Circle(Shape):          # inheritance
+#     def area(self):           # method overriding
+#         print("Circle area")
+
+# class Rectangle(Shape):       # inheritance
+#     def area(self):           # method overriding
+#         print("Rectangle area")
+
+# c = Circle()                  # object calling
+# r = Rectangle()               # object calling
+
+# c.area()
+# r.area()
+
+#problem 5
+
+class BANK:
+    def int(self):
+        print("Bank")
+
+class HBL:
+    def int(self):
+        print("HBL interest rate")
+class UBL:
+    def int(self):
+        print("UBL Interest")     
+h=HBL()
+u=UBL()      
+h.int()
+u.int()
+
